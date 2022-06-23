af616af9d81b0cb37d56b4d3596a76106fffba71
astergabedev <gabiferpub@gmail.com>
2022-06-23 16:54:01 -0300

    find attraction now working

--- script.py
+++ script.py
@@ -8,7 +8,7 @@ def get_destination_index(destination):
     destination_index = destinations.index(destination)
     return destination_index
 
-print(get_destination_index("Paris, France"))
+#print(get_destination_index("Paris, France"))
 
 def get_traveler_location(traveler):
   traveler_destination = traveler[1]
@@ -16,10 +16,10 @@ def get_traveler_location(traveler):
   return traveler_destination_index
 
 test_destination_index = get_traveler_location(test_traveler)
-print(test_destination_index)
+#print(test_destination_index)
 
 attractions = [[] for destin in destinations]
-print(attractions)
+#print(attractions)
 
 def add_attraction(destination, attraction):
     destination_index = get_destination_index(destination)
@@ -38,4 +38,24 @@ add_attraction("São Paulo, Brazil", ["São Paulo Zoo", ["zoo"]])
 add_attraction("São Paulo, Brazil", ["Pátio do Colégio", ["historical site"]])
 add_attraction("Cairo, Egypt", ["Pyramids of Giza", ["monument", "historical site"]])
 add_attraction("Cairo, Egypt", ["Egyptian Museum", ["museum"]])
-print(attractions)
+#print(attractions)
+
+def find_attractions(destination, interests):
+    destination_index = get_destination_index(destination)
+    attractions_in_city = attractions[destination_index]
+    attractions_with_interest = []
+    for attraction in attractions_in_city:
+        possible_attraction = attraction
+        attraction_tags = attraction[1]
+
+        for interest in interests:
+            if interest in attraction_tags:
+                attractions_with_interest.append(possible_attraction)
+                return attractions_with_interest
+
+la_arts = find_attractions("Los Angeles, USA", ['art'])
+print(la_arts)
+
+
+
+
